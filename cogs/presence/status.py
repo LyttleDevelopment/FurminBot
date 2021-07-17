de46f8cf49ab8b4a6c07cce30ec3884d695e6857
Kilian De Bock <Stualyttle@users.noreply.github.com>
2021-07-17 18:49:17 +0200

    Update 21.28.1

--- cogs/presence/status.py
+++ cogs/presence/status.py
@@ -1,8 +1,8 @@
 import discord
 from discord import Member
 from discord.ext import commands, tasks
+from utils.config import config
 from itertools import cycle
-from utils.config_presence import config_presence
 
 
 class Status(commands.Cog):
@@ -10,15 +10,19 @@ class Status(commands.Cog):
         self.client = client
         self.change_status.start(client)
 
-    status = cycle(config_presence.status)
-    status_nr = cycle(config_presence.status_nr)
+    status = cycle(config.status)
+    status_nr = cycle(config.status_nr)
 
     @tasks.loop(seconds=20)
     async def change_status(self, client):
-        await self.client.change_presence(status=discord.Status.online, activity=discord.Activity(name=f"{next(self.status)}", type=next(self.status_nr)))
+        # await self.client.change_presence(activity=discord.Game(next(self.status)))
+        await self.client.change_presence(
+            status=discord.Status.dnd,
+            activity=discord.Activity(
+                name=f"{next(self.status)}", type=next(self.status_nr)
+            ),
+        )
 
 
 def setup(client):
     client.add_cog(Status(client))
-
-
