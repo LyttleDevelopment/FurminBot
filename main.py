281b8e9592432d0ea4214b74aecc67896b4fb495
Kilian De Bock <Stualyttle@users.noreply.github.com>
2021-07-17 18:46:06 +0200

    Update 21.28.1

--- main.py
+++ main.py
@@ -26,32 +26,65 @@ def try_cog_action(function):
 # Actions when bot loaded
 @client.event
 async def on_ready():
-    logs_channel = client.get_channel(856832351245828116)
-    await logs_channel.send(f"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"
-                            f"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_")
-    await logs_channel.send(f"> **<@{client.user.id}> MAIN:** Bot starting, please wait!")
-    await logs_channel.send(f"> **<@{client.user.id}> MAIN:** Production: {str(prod)}")
-    # Process loading.
+    # Set status to booting.
+    await client.change_presence(
+        status=discord.Status.dnd, activity=discord.Activity(name="booting...", type=5)
+    )
+
+    # Reset log grouping messages.
+    log_print = [
+        f"{line}",
+        f"> **<@{client.user.id}> MAIN:** Bot starting, please wait!",
+        f"> **<@{client.user.id}> MAIN:** Production: {str(prod)}",
+    ]
+
+    # Start loading cogs.
     print("MAIN: Loading cogs...")
-    await logs_channel.send(f"> **<@{client.user.id}> MAIN:** Loading cogs...")
+    log_print.append(f"> **<@{client.user.id}> MAIN:** Loading cogs...")
+
+    # Merge messages and send to log channel.
+    log_message = "\n".join(map(str, log_print))
+    await main_log(client, log_message)
+
+    # Reset log grouping messages.
+    log_print = []
+
     # Set values for loading cogs.
     total_successful_loads = 0
     total_cumulative_files = 0
+
     # Load cogs.
     for cog_folder in cog_folders:
-        successful_loads, total_files = cog_loader(cog_folder)
+        successful_loads, total_files, status = cog_loader(cog_folder)
         # Prints:
-        print(
-            f"COGS: cogs.{cog_folder} - {successful_loads}/{total_files} succeeded."
+        print(f"COGS: cogs.{cog_folder} - {successful_loads}/{total_files} succeeded.")
+        log_print.append(
+            f"> **<@{client.user.id}> COGS:** ***cogs.{cog_folder} - {successful_loads}/{total_files} succeeded.***"
         )
-        await logs_channel.send(f"> **<@{client.user.id}> COGS:** cogs.{cog_folder} - {successful_loads}/{total_files} succeeded.")
+        status = "\n".join(map(str, status))
+        log_print.append(f"{status}")
         total_successful_loads += successful_loads
         total_cumulative_files += total_files
-    # Final messages.
-    print(f"COGS: Successfully loaded {total_successful_loads}/{total_cumulative_files} cogs.")
+
+    # Merge messages and send to log channel.
+    log_message = "\n".join(map(str, log_print))
+    await main_log(client, log_message)
+
+    # Print ready state.
+    print(
+        f"COGS: Successfully loaded {total_successful_loads}/{total_cumulative_files} cogs."
+    )
     print("MAIN: Cogs loaded, bot is active!")
-    await logs_channel.send(f"> **<@{client.user.id}> COGS:** Successfully loaded {total_successful_loads}/{total_cumulative_files} cogs.")
-    await logs_channel.send(f"> **<@{client.user.id}> MAIN:** Cogs loaded, bot is active <@132487290835435521>!")
+
+    # Reset log grouping messages.
+    log_print = [
+        f"> **<@{client.user.id}> COGS:** Successfully loaded {total_successful_loads}/{total_cumulative_files} cogs.",
+        f"> **<@{client.user.id}> MAIN:** Cogs loaded, bot is active!",
+    ]
+
+    # Merge messages and send to log channel.
+    log_message = "\n".join(map(str, log_print))
+    await main_log(client, log_message)
 
 
 # Cog folders currently active.
@@ -59,21 +92,26 @@ cog_folders: List[str] = ["core", "development", "presence", "actions", "command
 
 
 def cog_loader(folder: str):
-    files: List[str] = os.listdir(f'cogs/{folder}')
-    files = list(filter(lambda f: f.endswith('.py'), files))
+    files: List[str] = os.listdir(f"./cogs/{folder}")
+    files = list(filter(lambda f: f.endswith(".py"), files))
     successful_loads = 0
+    status = []
 
     for filename in files:
         cog_name = f"cog.{folder}.{filename[:-3]}"
 
         try:
-            client.load_extension(f'cogs.{folder}.{filename[:-3]}')
-            print(f"COGS: Loaded \"{cog_name}\"")
+            client.load_extension(f"cogs.{folder}.{filename[:-3]}")
+            print(f'COGS: Loaded "{cog_name}"')
+            status.append(f'> **<@{client.user.id}> COGS:** Loaded "{cog_name}"')
             successful_loads += 1
         except Exception as e:
-            print(f"ERROR loading extension {cog_name}:\n{e}")
+            print(f"ERHR: ERROR loading extension {cog_name}:\n{e}")
+            status.append(
+                f"> **<@{client.user.id}> ERHR:** ERROR loading extension {cog_name}:\n```{e}```"
+            )
 
-    return successful_loads, len(files)
+    return successful_loads, len(files), status
 
 
 @client.command()
